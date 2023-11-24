775a85455a031a20ce51066e8a2d42d4fae37259
4b1g0069 <4b1g0069@office.stust.edu.tw>
2023-11-24 10:59:02 +0800

    1124_3

--- test.py
+++ test.py
@@ -1,22 +1,27 @@
+class Course:
+  def __init__(self, course_name, 
+               course_id, course_credits):
+    self.course_name = course_name
+    self.course_id = course_id
+    self.course_credits = course_credits
+
+    def __str__(self):
+      return f"Course Name: {self.course_name}\nCourse ID = {self.course_id}\nCourse credits = {self.course_credits}"
+
 class Student:
-    def _init_(self, stu_name , stu_id):
-        self.stu_name = stu_name
-        self.stu_id = stu_id
-    
-    def _stf_(self):
-       return f"Name:{self.stu_name}\nId={self.stu_id}"
-    def addCourse(self , object)
+  def __init__(self, stu_name, stu_id):
+    self.stu_name = stu_name
+    self.stu_id = stu_id
 
-class Person:
-    def _init_(self, name, age):
-        self.name = name
-        self.age = age
+  def __str__(self):
+    return f"Name: {self.stu_name}\nID = {self.stu_id}"
+  
+  def addCourse(self, object):
+    self.course = object
 
-    def _str_(self):
-        return f"Name:{self.name}\nAge={self.age}"
-
-p1 = Person("John",36)
-print(p1)
-
-p2 = Person("Sue",22)
-print(p2._dict_)
+c1 = Course(course_name="套裝軟體應用", 
+            course_id="(G0D17M01",
+            course_credits=3)
+s1 = Student(stu_name="林旻諺", stu_id="4b1g0069")
+s1.addCourse(c1)
+print(s1)
